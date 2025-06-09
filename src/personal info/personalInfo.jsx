219a13cbcb39e59dc2bb29aed8e3d413028117ee
Legend Paul <qamunyamaina950@gmail.com>
2025-06-09 19:42:15 +0300

    Create a Accordion to open and close Sections

--- src/personal info/personalInfo.jsx
+++ src/personal info/personalInfo.jsx
@@ -1,18 +1,59 @@
-import { Input, TextArea } from "../utils/reusableIU";
+import { useState } from "react";
+
+import { Input, Button, Accordion } from "../utils/reusableIU";
 import "./personalInfo.css";
 import "../index.css";
 
 function PersonalInfo() {
+    const [isOpen, setIsOpen] = useState(true);
+    const toggleAccordion = () => setIsOpen(!isOpen);
     return (
         <section className="personal-info">
-            <h2>Personal Information</h2>
-            <div className="input-fiel">
+            <Accordion
+                sectionName={"Personal Information"}
+                isOpen={isOpen}
+                onClick={toggleAccordion}
+            />
+            {isOpen && <PersonalInfoFields />}
+        </section>
+    );
+}
+
+function PersonalInfoFields() {
+    const addNewField = () => {
+        return (
+            <select>
+                <option value="text">Text Field</option>
+                <option value="email">Email Field</option>
+                <option value="date">Date Field</option>
+                <option value="number">Number Field</option>
+                <option value="tel">Tel Filed</option>
+            </select>
+        );
+    };
+    return (
+        <div className="info-fields">
+            <div className="input-field">
                 <Input inputTitle="Name" type={"text"} />
                 <Input inputTitle="Email" type={"email"} />
                 <Input inputTitle="Phone" type={"tel"} />
                 <Input inputTitle="Address" type={"text"} />
             </div>
-        </section>
+            <div className="buttons-container">
+                <Button
+                    buttonTitle="Add New Field"
+                    bgc={"var(--moderate-green)"}
+                    onClick={addNewField}
+                />
+                <Button
+                    buttonTitle="Save Details"
+                    bgc={"var(--blue)"}
+                    onClick={() => {
+                        console.log("Add button clicked");
+                    }}
+                />
+            </div>
+        </div>
     );
 }
 
--- src/utils/reusableIU.jsx
+++ src/utils/reusableIU.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./reusableIU.css";
 
+// Input Componetc
 function Input({ inputTitle, type }) {
     const [value, setValue] = useState("");
     return (
@@ -16,6 +17,8 @@ function Input({ inputTitle, type }) {
         </>
     );
 }
+
+// TextArea Component
 function TextArea({ textareaTitle }) {
     const [value, setValue] = useState("");
     return (
@@ -31,4 +34,31 @@ function TextArea({ textareaTitle }) {
     );
 }
 
-export { Input, TextArea };
+// Button Component
+function Button({ buttonTitle, bgc, onClick }) {
+    return (
+        <button
+            className="button"
+            style={{ backgroundColor: bgc }}
+            onClick={onClick}
+        >
+            {buttonTitle}
+        </button>
+    );
+}
+
+// Arccodion Componenet
+function Accordion({ sectionName, isOpen, onClick }) {
+    return (
+        <div className="pannel" onClick={onClick}>
+            <h2>{sectionName}</h2>
+            {isOpen ? (
+                <i class="bi bi-chevron-down"></i>
+            ) : (
+                <i class="bi bi-chevron-right"></i>
+            )}
+        </div>
+    );
+}
+
+export { Input, TextArea, Button, Accordion };
