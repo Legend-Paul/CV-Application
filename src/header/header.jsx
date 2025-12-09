e11245c726aa0c0aab8f333c6c0517d7f5446c42
Legend-Paul <qamunyamaina950@gmail.com>
2025-12-09 14:52:26 +0300

    Add Save cv and load icons from localstorage

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -2,28 +2,28 @@ import "../index.css";
 import "./header.css";
 import image from "../images/noBgLogo.png";
 
-function Header() {
-    return (
-        <>
-            <header className="header">
-                <div className="header-container">
-                    <div className="logo">
-                        <img
-                            src={image}
-                            alt="CV Icon"
-                            className="header-icon"
-                        />
-                    </div>
-                    <div className="header-text">
-                        <h1>
-                            {" "}
-                            <abbr title="CURRICULUM VITAE">CV</abbr> APPLICATION
-                        </h1>
-                        <p>Fill out the form below to create your CV</p>
-                    </div>
-                </div>
-            </header>
-        </>
-    );
+function Header({ handleSaveCV, handleLoadCV }) {
+  return (
+    <>
+      <header className="header">
+        <div className="header-container">
+          <div className="logo">
+            <img src={image} alt="CV Icon" className="header-icon" />
+          </div>
+          <div className="header-text">
+            <h1>
+              {" "}
+              <abbr title="CURRICULUM VITAE">CV</abbr> APPLICATION
+            </h1>
+            <p>Fill out the form below to create your CV</p>
+          </div>
+          <div className="save-cv-icons">
+            <i class="bi bi-bookmark" onClick={handleSaveCV}></i>
+            <i class="bi bi-file-earmark-check-fill" onClick={handleLoadCV}></i>
+          </div>
+        </div>
+      </header>
+    </>
+  );
 }
 export default Header;
